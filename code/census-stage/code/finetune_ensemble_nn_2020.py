29f8edae2fc286e8ebc446c418041fdc5cc7c762
MarivelXYZ <79127675+MarivelXYZ@users.noreply.github.com>
2026-01-19 13:27:45 -0600

    Updatecode

--- code/census-stage/code/finetune_ensemble_nn_2020.py
+++ code/census-stage/code/finetune_ensemble_nn_2020.py
@@ -11,9 +11,9 @@ import os
 
 
 # Set relevant directory locations
-common_data_dir = '../INEGI_CPV2020_n9/'
-global_data_dir = '../data/'
-models_dir = '../models/'
+common_data_dir = '../../../../data/INEGI_CPV2020_n9/'
+global_data_dir = '../../../../data/'
+models_dir = '../../../../models/'
 
 # Define the Neural Network model (same as used in the individual NN model)
 class Net(nn.Module):
